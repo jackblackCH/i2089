15e292a9ec4f30d9cdbbcdc4071382fb98d90311
Marc Illien <marc.illien@gmail.com>
2024-06-03 01:28:03 +0200

    Taking off

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "i2089",
+  description: "i2089 - Experience the future of the web",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-svh">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex h-svh flex-col items-center justify-center p-24">
+      <div className="text-3xl">i2089</div>
+    </main>
+  );
+}
